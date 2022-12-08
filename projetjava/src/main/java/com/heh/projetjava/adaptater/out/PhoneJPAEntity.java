5a76a267ec4185d9aacbcaefefa2946896b93e35
WaNNTaD <julien.deprez10@gmail.com>
2022-12-08 12:01:08 +0100

    affichage données  fonctionnel

--- projetjava/src/main/java/com/heh/projetjava/adaptater/out/PhoneJPAEntity.java
+++ projetjava/src/main/java/com/heh/projetjava/adaptater/out/PhoneJPAEntity.java
@@ -11,7 +11,7 @@ import javax.persistence.*;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-@Table(name = "Telephone")
+@Table(name = "phones")
 public class PhoneJPAEntity {
 
     @Id
---
+++ projetjava/src/main/resources/db/migration/V1__DBCreator.sql
@@ -0,0 +1,7 @@
+CREATE TABLE phones(
+      id SERIAL PRIMARY KEY,
+      marque VARCHAR(50) NOT NULL,
+      modele VARCHAR(15) NOT NULL,
+      capacite INTEGER NOT NULL
+)
+
