4242727eacb96c197fc8d45d5df43d05d1afac2d
WaNNTaD <julien.deprez10@gmail.com>
2023-01-18 19:10:09 +0100

    Programme dans le main

--- projetjava/src/main/java/com/heh/projetjava/adaptater/out/PhoneRepository.java
+++
@@ -1,8 +0,0 @@
-package com.heh.projetjava.adaptater.out;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PhoneRepository extends JpaRepository<PhoneJPAEntity,Long> {
-
-}
--- projetjava/src/main/resources/db/migration/V1__DBCreator.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE phones(
-      id SERIAL PRIMARY KEY,
-      marque VARCHAR(50) NOT NULL,
-      modele VARCHAR(15) NOT NULL,
-      capacite INTEGER NOT NULL
-)
-
